6e2833903d06bfa7daf3c2766d55f4a275abe3ed
grommoott <grommoott@gmail.com>
2024-12-09 20:12:13 +0700

    rollup options

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, Rollup, rollupVersion } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), tsconfigPaths()],
+    build: {
+        rollupOptions: {
+            external: "./node_modules/"
+        }
+    }
 })
